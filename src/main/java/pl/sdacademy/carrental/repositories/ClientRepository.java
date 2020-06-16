e5e110fd82f05b30632eb1eedd4da3dbb69dd376
Your Name <piotrkowalik246@gmail.com>
2020-06-16 21:23:13 +0200

    //Done similarly to branches to be consistent -Added user register functionality, edit user, delete user (TODO refactor to make it not only admin usage) -Home button defined in components.html now redirects to home page

---
+++ src/main/java/pl/sdacademy/carrental/repositories/ClientRepository.java
@@ -0,0 +1,8 @@
+package pl.sdacademy.carrental.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.sdacademy.carrental.domain.Client;
+
+
+public interface ClientRepository extends JpaRepository<Client,Long> {
+}
---
+++ src/main/java/pl/sdacademy/carrental/services/ClientService.java
@@ -0,0 +1,88 @@
+package pl.sdacademy.carrental.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.sdacademy.carrental.domain.Address;
+import pl.sdacademy.carrental.domain.Client;
+import pl.sdacademy.carrental.model.ClientForm;
+import pl.sdacademy.carrental.repositories.AddressRepository;
+import pl.sdacademy.carrental.repositories.ClientRepository;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ClientService {
+
+    private final ClientRepository clientRepository;
+    private final AddressRepository addressRepository;
+
+    public ClientService(ClientRepository clientRepository, AddressRepository addressRepository) {
+        this.clientRepository = clientRepository;
+        this.addressRepository = addressRepository;
+    }
+
+    public List<Client> getAll() {
+        return clientRepository.findAll();
+    }
+
+    public void createClient(final ClientForm clientForm) {
+
+        final Address address = createAddressFromClientForm(clientForm);
+
+        final Client client = Client.builder()
+                .firstName(clientForm.getFirstName())
+                .lastName(clientForm.getLastName())
+                .email(clientForm.getEmail())
+                .address(address)
+                .phoneNumber(clientForm.getPhoneNumber())
+                .build();
+        clientRepository.save(client);
+    }
+
+    public void delete(final Long id) {
+        final Client client = clientRepository.findById(id).orElseThrow();
+        clientRepository.delete(client);
+    }
+
+    public void updateClient(final Long id, final ClientForm clientForm) {
+
+        final Address address = createAddressFromClientForm(clientForm);
+
+        final Client client = clientRepository.findById(id).orElseThrow();
+        client.setFirstName(clientForm.getFirstName());
+        client.setLastName(clientForm.getLastName());
+        client.setAddress(address);
+        clientRepository.save(client);
+    }
+
+    private Address createAddressFromClientForm(final ClientForm clientForm){
+        final Address address = Address.builder()
+                .city(clientForm.getCity())
+                .street(clientForm.getStreet())
+                .building(clientForm.getBuilding())
+                .apartment(clientForm.getApartment())
+                .zipCode(clientForm.getZipCode())
+                .build();
+        return addressRepository.save(address);
+    }
+    // I don't know if saving both branch and client address to the same repository is a good idea
+    // Split, refactor AddressRepository to ClientAddressRepository and BranchAddressRepository?
+    // same with Address entity? What do you think?
+
+    public ClientForm getById(final Long id) {
+        final Client client = clientRepository.findById(id).orElseThrow();
+        final Address address = client.getAddress();
+        return ClientForm.builder()
+                .firstName(client.getFirstName())
+                .lastName(client.getLastName())
+                .city(address.getCity())
+                .street(address.getStreet())
+                .apartment(address.getApartment())
+                .zipCode(address.getZipCode())
+                .phoneNumber(client.getPhoneNumber())
+                .build();
+
+    }
+
+}
