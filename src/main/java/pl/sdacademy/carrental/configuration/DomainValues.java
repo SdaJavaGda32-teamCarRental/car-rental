957f227c62fcdccc93fa41d7b47b370d67c8ff45
MNowicki87 <nowicki.pl@gmail.com>
2020-06-18 19:35:51 +0200

    - DomainValues -> back to class (final)

--- src/main/java/pl/sdacademy/carrental/configuration/DomainValues.java
+++ src/main/java/pl/sdacademy/carrental/configuration/DomainValues.java
@@ -1,8 +1,9 @@
 package pl.sdacademy.carrental.configuration;
 
 // TODO: 18/06/2020 it would probably be better to store these values in DB
-public interface DomainValues {
-   int UPCHARGE_FOR_CHANGE_OF_BRANCH = 50;
-   int MINIMUM_CARS_IN_BRANCH = 3;
+public final class DomainValues {
+   public static final int UPCHARGE_FOR_CHANGE_OF_BRANCH = 50;
+   public static final int MINIMUM_CARS_IN_BRANCH = 3;
    
+   private DomainValues() {};
 }
